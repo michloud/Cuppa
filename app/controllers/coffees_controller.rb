e50946a144e7ed4bf3f4ebd1f0608ee87da30818
michloud <chinoah96@gmail.com>
2019-09-07 15:00:02 +0000

    add and style about page

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -2,4 +2,8 @@ class CoffeesController < ApplicationController
   def index
     @coffees = Caffeine.all
   end
+
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'coffees#index'
   resources :coffees
+  get 'about', to: 'coffees#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
