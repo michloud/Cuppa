1f86358f1e021be40dd134010de46f8de68ad3e0
michloud <chinoah96@gmail.com>
2019-09-07 17:00:53 +0000

    pagination

--- Gemfile
+++ Gemfile
@@ -69,6 +69,10 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
+gem 'will_paginate', '~> 3.1.0'
+gem 'will_paginate-bootstrap'
+
+
 
 
 
--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,9 +1,12 @@
 class CoffeesController < ApplicationController
   def index
-    @coffees = Caffeine.all
+    @coffees = Caffeine.paginate(:page =>params[:page], :per_page => 3)
   end
 
 
   def about
   end
+  
+
+
 end
