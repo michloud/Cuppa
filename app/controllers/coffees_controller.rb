704f93a6493b23e86802bdcbfac39e9e4e55b5bb
michloud <chinoah96@gmail.com>
2019-09-08 17:21:41 +0000

    page styling, fix font size to box

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,6 +1,6 @@
 class CoffeesController < ApplicationController
   def index
-    @coffees = Caffeine.paginate(:page =>params[:page], :per_page => 3)
+    @coffees = Caffeine.paginate(:page =>params[:page], :per_page => 4)
   end
 
 
