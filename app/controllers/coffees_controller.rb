3e5ffba0626fadb321f44f03c342670ac7198cca
michloud <chinoah96@gmail.com>
2019-10-02 19:28:41 +0000

    Link submit button to database and redirect to home page

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -12,7 +12,16 @@ class CoffeesController < ApplicationController
 
   end
   
+  def create
+    Caffeine.create(coffee_params)
+    redirect_to root_path
+  end
 
+  private
+
+  def coffee_params
+    params.require(:caffeine).permit(:name, :country, :description)
+  end
 
 end
 
