0452870d9fad7e48a3029ee21aac68fdbdfb427f
michloud <chinoah96@gmail.com>
2019-10-23 17:02:31 +0000

    move new form to modal, delete new views,update controller show method to include new form,move modal box to navbar and style

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,21 +1,19 @@
 class CoffeesController < ApplicationController
   def index
     @coffees = Caffeine.all.paginate(:page => params[:page], :per_page => 4)
+    @coffee = Caffeine.new
   end
-
 
   def about
   end
 
-  def new
-    @coffee = Caffeine.new
-
-  end
   
   def create
     Caffeine.create(coffee_params)
     redirect_to root_path
   end
+
+
 
   private
 
